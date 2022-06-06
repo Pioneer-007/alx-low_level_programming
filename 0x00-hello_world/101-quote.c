6eabb0e36e3b789ee401e8934e297f3d1213ce3e
okogun celina <cokogun@gmail.com>
2022-06-06 16:03:02 -0700

    my commit message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <studio>
+/**
+ *main - Entry point
+ *
+ *Return: 1 after printing the function
+ */
+int main(void)
+{
+puts("\"and that piece of art is useful" - Dora Korpar, 2015-10-19");
+return (0);
+}
