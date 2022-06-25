9155a8823dccdb9987d48d510f4500e2a86a3ad2
Okogun Celina Blessing <cokogun@gmail.com>
2022-06-25 07:28:14 +0000

    3. Neither irony nor sarcasm is argument

---
+++ 3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main -> this is a function to print to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	print("%d\n", result);
+	return (0);
+}
