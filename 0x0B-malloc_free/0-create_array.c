dde28c571a9a26d13153b7769de2fccda0741fb6
Okogun Celina Blessing <cokogun@gmail.com>
2022-06-28 05:02:29 +0000

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  * @c: character an array is initialized with
  * Return: a pointer to an array
  */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *s;
