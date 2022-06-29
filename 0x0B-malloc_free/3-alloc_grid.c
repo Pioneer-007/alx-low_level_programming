e14dc3d9c39da320b2b8c0f8530361bb4965810f
Okogun Celina Blessing <cokogun@gmail.com>
2022-06-29 00:34:16 +0000

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * alloc_grid -> allocating 2d array
+ * @width: width of the array
+ * @height: height of the array
+ * Return: a pointer to a allocoted grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int **twoD;
+	int hgt_index, wid_index;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	twoD = malloc(sizeof(int *) * height);
+
+	if (twoD == NULL)
+		return (NULL);
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+
+		if (twoD[hgt_index] == NULL)
+		{
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
+
+			free(twoD);
+			return (NULL);
+		}
+	}
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
+	}
+
+	return (twoD);
+}
